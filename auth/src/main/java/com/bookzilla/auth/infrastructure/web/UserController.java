97eff606d191861903bd00903e575274bdc9d1a6
Volodymyr Metlyakov <v.metlyakov@mascode.com>
2026-09-18 13:09:39 +0300

    added @Valid annotation to the project (just for fun and test)

--- auth/src/main/java/com/bookzilla/auth/infrastructure/web/UserController.java
+++ auth/src/main/java/com/bookzilla/auth/infrastructure/web/UserController.java
@@ -2,6 +2,7 @@ package com.bookzilla.auth.infrastructure.web;
 
 import com.bookzilla.auth.application.service.UserService;
 import com.bookzilla.auth.infrastructure.web.dto.RegisterRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +19,7 @@ public class UserController {
     }
 
     @PostMapping("/api/auth/register")
-    public void register(@RequestBody RegisterRequest registerRequest){
+    public void register(@Valid @RequestBody RegisterRequest registerRequest){
         userService.register(registerRequest.firstName(),
                             registerRequest.lastName(),
                             registerRequest.password(),
