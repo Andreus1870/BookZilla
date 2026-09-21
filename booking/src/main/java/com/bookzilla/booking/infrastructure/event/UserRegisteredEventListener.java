fd1ed8da851666456cdbf8967e42794558bce994
Andreus <metlyakov.andrii@gmail.com>
2026-09-21 09:53:29 +0200

    feat(booking): create Client on user registration

--- booking/src/main/java/com/bookzilla/booking/infrastructure/event/UserRegisteredEventListener.java
+++ booking/src/main/java/com/bookzilla/booking/infrastructure/event/UserRegisteredEventListener.java
@@ -1,5 +1,6 @@
 package com.bookzilla.booking.infrastructure.event;
 
+import com.bookzilla.booking.application.service.ClientService;
 import com.bookzilla.booking.application.service.ProviderService;
 import com.bookzilla.contracts.event.UserRegistered;
 import org.springframework.stereotype.Component;
@@ -10,13 +11,16 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class UserRegisteredEventListener {
 
     private final ProviderService providerService;
+    private final ClientService clientService;
 
-    public UserRegisteredEventListener(ProviderService providerService) {
+    public UserRegisteredEventListener(ProviderService providerService, ClientService clientService) {
         this.providerService = providerService;
+        this.clientService = clientService;
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleUserRegistered(UserRegistered event) {
         providerService.createProvider(event.userId());
+        clientService.createClient(event.userId());
     }
 }
